57f8799d2015fa3cb4a3d341ed9bfd6037576842
cXVxZ <102743782+cXVxZ@users.noreply.github.com>
2022-09-01 21:05:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds feeds/mtk_openwrt_feed
+feeds/mtk_openwrt_feed/prepare_sdk.sh
